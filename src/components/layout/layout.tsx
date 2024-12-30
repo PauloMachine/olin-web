224dd0b8e328c38ae53b45a7f668818a5152e0d2
Paulo Guilherme Santiago <37718188+PauloMachine@users.noreply.github.com>
2024-12-30 16:20:10 -0300

    feat: add isLogged

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,20 +2,24 @@ import type { ReactNode } from "react";
 import Navbar from "../navbar";
 import Flex from "../ui/flex";
 import { useAuth } from "src/features/auth/auth.context";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const location = useLocation();
   const { isAuthenticated } = useAuth();
+
+  const isLogged = isAuthenticated && !location.pathname.includes("/login");
 
   return (
     <Flex
       gap="50"
-      justify={isAuthenticated ? "flex-start" : "center"}
+      justify={isLogged ? "flex-start" : "center"}
       style={{
         minHeight: "100vh",
-        backgroundColor: isAuthenticated ? "#ffffff" : "#8338E7",
+        backgroundColor: isLogged ? "#ffffff" : "#8338E7",
       }}
     >
-      {isAuthenticated && <Navbar />}
+      {isLogged && <Navbar />}
       <Flex px="100" style={{ paddingBottom: 50 }}>
         {children}
       </Flex>
